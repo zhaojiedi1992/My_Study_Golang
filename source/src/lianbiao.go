baa0898b0739c3b366c1aa887046b835c6e28c75
zhaojiedi <zhaojiedi@duxiaoman.com>
2019-12-23 16:19:18 +0800

    add jiegouti

---
+++ source/src/lianbiao.go
@@ -0,0 +1,27 @@
+package main
+import "fmt"
+
+
+type Node struct {
+    data int
+    next *Node
+}
+
+func ShowNode(p *Node)  {
+	for p !=nil {
+		fmt.Println(*p)
+        p = p.next //移动指针
+	}
+}
+
+func main() {
+    var head = new(Node)
+    head.data = 1
+    var node1 = new(Node)
+    node1.data = 2
+    head.next = node1
+    var node2 = new(Node)
+    node2.data = 3
+    node1.next = node2
+    Shownode(head)
+}
